ee88675b7d3cf15de15f5db28d5abfe4aacb254a
DannielWang <wangdian201810@gmail.com>
2023-12-14 15:32:54 +0800

    test

--- api/apikeys.py
+++ api/apikeys.py
@@ -3,27 +3,38 @@ from flask import request, jsonify
 from common.sha265_operate import decrypt
 from common.sha265_operate import encrypt
 import secrets
+from storage import api_key_operation
 
-password = "412312"
+
+# password = "412312"
 
 
 def auth():
-    sha256_token = request.values.get("sha", "").strip()
-    key = decrypt(sha256_token, password)
-    with open("api_key.txt", 'r', encoding="utf-8") as f:
-        for line in f:
-            if line == key:
-                return jsonify({""}), 200
-            else:
-                return jsonify({""}), 401
+    try:
+        token = request.values.get("key", "").strip()
+    except Exception:
+        return jsonify({"msg": "no API Key been received"}), 401
+    # key = decrypt(sha256_token, password)
+    if api_key_operation.compare_api_key(token):
+        return jsonify({"msg": ""}), 200
+    else:
+        return jsonify({"msg": "no matched Key"}), 401
 
 
 def generate_api_key():
-    token = secrets.token_hex(16)
-    print(token)
-    file = open("api_key.txt", 'w')
-    file.write(token)
-    file.close()
-    sha256_token = encrypt(token.encode(), password)
+    usr = request.values.get("usr", "").strip()
+    try:
+        token = secrets.token_hex(16)
+    except Exception:
+        return jsonify({"msg": "failed to generate the API Key"}), 401
+    try:
+        api_key_operation.save_api_key(usr, token)
+    except Exception:
+        return jsonify({"msg": "failed to save the key"}), 401
 
-    return sha256_token
+    # try:
+    #     sha256_token = encrypt(token.encode(), password)
+    # except Exception:
+    #     return jsonify({"failed to encrypt"}), 401
+
+    return token
--- route.py
+++ route.py
@@ -4,23 +4,6 @@ import api.apikeys
 
 
 def init():
-    # app.add_url_rule("/get", methods=["POST"], view_func=api.user.get_user)
-    # app.add_url_rule("/list", methods=["POST"], view_func=api.user.get_all_users)
-    # app.add_url_rule("/register", methods=["OPTIONS"], view_func=api.user.user_register)
-    # app.add_url_rule("/login", methods=["POST"], view_func=api.user.user_login)
-    # app.add_url_rule("/update", methods=["POST"], view_func=api.user.user_update)
-    # app.add_url_rule("/delete", methods=["POST"], view_func=api.user.user_delete)
-    # app = Flask(__name__)
-    # auth
-
-    # @app.route('/generate', methods=['POST'])
-    # def generate():
-    #     api.apikeys.generate_api_key()
-    #
-    # @app.route('/auth', methods=['POST'])
-    # def auth():
-    #     data = request.form.get("sha")
-    #     api.apikeys.auth(data)
     app.add_url_rule("/generate", methods=["POST"], view_func=api.apikeys.generate_api_key)
     app.add_url_rule("/auth", methods=["POST"], view_func=api.apikeys.auth)
     return
